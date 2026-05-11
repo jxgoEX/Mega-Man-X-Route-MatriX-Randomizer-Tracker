cc8a8d45b8a2f866a6e3900bb5eb2c6593d75515
jxgoEX <broanEX@proton.me>
2026-05-10 23:58:48 -0400

    clean the fork, prepare the plate

--- src/component/GridContainer.tsx
+++
@@ -1,8 +0,0 @@
-import { children, type JSX } from "solid-js";
-
-export default function GridContainer(props: { children: number | boolean | Node | JSX.ArrayElement | (string & {}) | null | undefined; cols: any; }) {
-  const c = children(() => props.children)
-  return <div class={`relative grid grid-flow-row grid-cols-${props.cols} items-center`}>
-    {c()}
-  </div>
-}
